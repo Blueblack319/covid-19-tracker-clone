b03f22a76e2a39ecd0a93b4e46fac2b01d88feb6
Yang Jae Hoon <didwogns789@naver.com>
2020-10-18 10:24:08 +0900

    Styling app like wireframe

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import './App.css';
 
 import {FormControl,
   Select,
-  MenuItem 
+  MenuItem,
+  Card,
+  CardContent,
 } from "@material-ui/core"
+
+import InfoBox from './InfoBox';
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -28,21 +32,31 @@ function App() {
 
   const handleCountryChanged = (event) => {
     const countryCode = event.target.value;
-    console.log(countryCode);
     setCountry(countryCode);
   }
 
   return (
     <div className="app">
-      <div className="app__header">
-        <h1>COVID-19 TRACKER</h1>
-        <FormControl className="app__dropdown">
-          <Select variant="outlined" value={country} onChange={handleCountryChanged}>
-            <MenuItem value="Worldwide" key="WORLD">Worldwide</MenuItem>
-            {countries.map((country) => <MenuItem value={country.value} key={country.name}>{country.name}</MenuItem>)}
-          </Select>
-        </FormControl>
+      <div class="app__left">
+        <div className="app__header">
+          <h1>COVID-19 TRACKER</h1>
+          <FormControl className="app__dropdown">
+            <Select variant="outlined" value={country} onChange={handleCountryChanged}>
+              <MenuItem value="Worldwide" key="WORLD">Worldwide</MenuItem>
+              {countries.map((country) => <MenuItem value={country.value} key={country.name}>{country.name}</MenuItem>)}
+            </Select>
+          </FormControl>
+        </div>
+        <div className="app__stats">
+          <InfoBox title="Coronavirus Cases" cases="2000" total="1.2k" />
+          <InfoBox title="Recovered Cases" cases="2000" total="1.2k" />
+          <InfoBox title="Death Cases" cases="2000" total="1.2k" />
+        </div>
       </div>
+      <Card class="app__right">
+        <h2>Case by Country</h2>
+        <h2>Worldwide new Cases</h2>
+      </Card>
     </div>
   );
 }
